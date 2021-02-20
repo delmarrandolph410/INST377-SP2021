580e08799f78865f8faa500b114049e467aa02ff
delmarrandolph410 <78332107+delmarrandolph410@users.noreply.github.com>
2021-02-19 19:04:16 -0500

    Moving on..

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bird = document.querySelector('.bird')
-  const gameDisplay = document.querySelector('.game-container')
-  const ground = document.querySelector('.ground')
+document.addEventListener('DOMContentLoaded' , () => {
+   const bird = document.querySelector('.bird')
+   const gameDisplay = document.querySelector('.game-container')
+   const ground = document.querySelector('.ground')
 
-  let birdLeft = 220
-  let birdBottom = 100
-  let gravity = 2
-  let isGameOver = false
-  let gap = 430
+   let birdLeft = 220
+   let birdBottom = 100
+   let gravity = 2
+   let isGameOver = false
+   let gap = 430
 
 
-  function startGame(){
-    birdBottom -= gravity
-    bird.style.bottom = birdBottom + 'px'
-    bird.style.left = birdLeft + 'px'
+   function startGame(){
+       birdBottom -= gravity
+       bird.style.bottom = birdBottom + 'px'
+       bird.style.left = birdLeft + 'px'
    }
-   let gameTimerId = setInterval(startGame, 20)
+   let timerId = setinterval(startGame, 20)
 
    function control(e){
        if (e.keyCode === 32){
