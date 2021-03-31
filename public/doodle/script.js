7a6d268497d075f25c91ca056bf5266a406d9eb4
delmarrandolph410 <78332107+delmarrandolph410@users.noreply.github.com>
2021-03-31 14:34:35 -0400

    Adjustments

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  const doodler = document.createElement('div');
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
 
-  function createDoodler() {
-    grid.appendChild(doodler);
-    doodler.classList.add('doodler')
-    doodler.style.left = '50px'
-  }
-  createDoodler()
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
 
-});
+    }
+    createDoodler()
+})
